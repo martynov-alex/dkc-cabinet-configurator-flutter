9ab3e2b3a201c22c5fef90c1574f0de5b2b91e61
Alex Martynov <martynov.alex@gmail.com>
2022-06-23 19:11:44 +0600

    first commit

---
+++ android/app/src/main/kotlin/ru/martynov/dkc_cabinet_configurator/dkc_cabinet_configurator/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.martynov.dkc_cabinet_configurator.dkc_cabinet_configurator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/temp/screens/temp_screen/temp_screen_model.dart
@@ -0,0 +1,5 @@
+import 'package:dkc_cabinet_configurator/features/temp/screens/temp_screen/temp_screen.dart';
+import 'package:elementary/elementary.dart';
+
+/// Model fol [TempScreen].
+class TempScreenModel extends ElementaryModel {}
